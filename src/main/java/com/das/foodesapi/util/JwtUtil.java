197f3e0e49bdaa27284ce5f1daa1b64adcf9a115
laxmandas77 <laxmandaspanchireddy9@gmail.com>
2025-05-20 22:25:08 +0530

    changes

--- src/main/java/com/das/foodesapi/util/JwtUtil.java
+++ src/main/java/com/das/foodesapi/util/JwtUtil.java
@@ -28,7 +28,7 @@ public class JwtUtil {
                 .setClaims(claiams)
                 .setSubject(subject)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10 ))//10 hours
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10 * 10))//10 hours
                 .signWith(SignatureAlgorithm.HS256,SECRET_KEY)
                 .compact();
     }
