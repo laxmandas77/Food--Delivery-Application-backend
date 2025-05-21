5b0d9ed407c61bb767ef241eb2b2e059fbecd42f
laxmandas77 <laxmandaspanchireddy9@gmail.com>
2025-05-21 14:59:50 +0530

    changes

--- src/main/java/com/das/foodesapi/config/SecurityConfig.java
+++ src/main/java/com/das/foodesapi/config/SecurityConfig.java
@@ -55,7 +55,7 @@ public class SecurityConfig {
 
     private UrlBasedCorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173","http://localhost:5174","https://foodcustomerdashboard.netlify.app/","https://foodadmindashboard.netlify.app/"));
+        config.setAllowedOrigins(List.of("https://foodcustomerdashboard.netlify.app/","https://foodadmindashboard.netlify.app/"));
         config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS","PATCH"));
         config.setAllowedHeaders(List.of("Authorization","Content-Type"));
         config.setAllowCredentials(true);
