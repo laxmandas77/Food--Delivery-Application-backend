97bf1c9f3a8f64f890695d08398c7fe9d445cb9e
laxmandas77 <laxmandaspanchireddy9@gmail.com>
2025-05-21 12:32:28 +0530

    changes

--- src/main/java/com/das/foodesapi/FoodesapiApplication.java
+++ src/main/java/com/das/foodesapi/FoodesapiApplication.java
@@ -2,8 +2,10 @@ package com.das.foodesapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories(basePackages = "com.das.foodesapi.repositry")
 public class FoodesapiApplication {
 
 	public static void main(String[] args) {
